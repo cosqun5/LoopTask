e80feea735e90fbcf08440511cb7d61ade496f68
cosqun5 <87088111+cosqun5@users.noreply.github.com>
2023-04-03 23:26:40 +0400

    first commit

---
+++ LoopTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LoopTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ededi daxil edin:");
+            int a = int.Parse(Console.ReadLine());
+            if (a > 10 && a < 20)
+            {
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine("Eded cutdur");
+                }
+                else
+                {
+                    Console.WriteLine("Eded tekdir");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Eded 10-20 aralığına daxil deyil. ");
+            }
+
+        }
+    }
+}
